74621b788dabe5a03079d9b140c89e69f4e14c8a
Ehsan Akbar <45014174+akbar-ehsaan@users.noreply.github.com>
2022-11-23 12:28:32 +0330

    Add files via upload

---
+++ Akka_Router/CalculatorActor.cs
@@ -0,0 +1,68 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka_Router
+{
+    public class CalculatorActor : ReceiveActor
+
+    {
+
+        public CalculatorActor()
+
+        {
+
+            Receive<Add>(add => HandleAddition(add));
+
+        }
+
+        public void HandleAddition(Add add)
+
+        {
+
+            Console.WriteLine($"{Self.Path} received the request: {add.Term1}+{add.Term2}");
+
+            Sender.Tell(new Answer(add.Term1 + add.Term2));
+
+        }
+
+    }
+    public class Add
+
+    {
+
+        public Add(double term1, double term2)
+
+        {
+
+            Term1 = term1;
+
+            Term2 = term2;
+
+        }
+
+        public double Term1;
+
+        public double Term2;
+
+    }
+
+    public class Answer
+
+    {
+
+        public Answer(double value)
+
+        {
+
+            Value = value;
+
+        }
+
+        public double Value;
+
+    }
+}
---
+++ Akka_Router/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Akka.Actor;
+using Akka.Routing;
+using Akka_Router;
+
+Console.WriteLine("Hello, World!");
+ActorSystem system = ActorSystem.Create("my-first-akka");
+
+//var calculatorProps = Props.Create<CalculatorActor>()
+
+
+var calculatorProps = Props.Create<CalculatorActor>()
+
+                            .WithRouter(new ConsistentHashingPool(4)
+
+                            .WithHashMapping(x =>
+
+                            {
+
+                                if (x is Add)
+
+                                {
+
+                                    return ((Add)x).Term1;
+
+                                }
+
+
+
+                                return x;
+
+                            }));//                        .WithRouter(new Akka.Routing.SmallestMailboxPool(4));
+
+var calculatorRef = system.ActorOf(calculatorProps, "calculator");
+
+var result1 = calculatorRef.Ask(new Add(10, 20)).Result as Answer;
+
+var result2 = calculatorRef.Ask(new Add(11, 30)).Result as Answer;
+
+var result3 = calculatorRef.Ask(new Add(12, 40)).Result as Answer;
+
+var result4 = calculatorRef.Ask(new Add(13, 10)).Result as Answer;
+
+var result5 = calculatorRef.Ask(new Add(14, 25)).Result as Answer;
+
+Console.WriteLine($"Result 1 : {result1.Value}");
+
+Console.WriteLine($"Result 2 : {result2.Value}");
+
+Console.WriteLine($"Result 3 : {result3.Value}");
+
+Console.WriteLine($"Result 4 : {result4.Value}");
+
+Console.WriteLine($"Result 5 : {result5.Value}");
+
+Console.Read();
+
+system.Terminate();
